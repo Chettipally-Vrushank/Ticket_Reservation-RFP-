4519194dea855579ab778b365cdeb8e18559ce61
Abhigovindula <abhigovindula@gmail.com>
2025-01-20 21:21:35 +0530

    stage1

--- app.py
+++ app.py
@@ -63,6 +63,10 @@ def logout():
     session.clear()
     return redirect(url_for("login"))
 
+
+
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Create database tables
